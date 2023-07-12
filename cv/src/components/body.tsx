ae30e81dcd36e0e21b0111d4026f0a30bc4e9de7
sam <none>
2023-07-12 12:15:45 +0200

    init toggle, better css organization

--- cv/src/components/body.tsx
+++ cv/src/components/body.tsx
@@ -1,16 +1,14 @@
 "use client";
 
 import toogleThemeColor from "@/js/theme_changer";
+import ToggleThemeChanger from "./toggle-theme-changer";
 
 function Body({ children }: {
 	children: null | React.ReactNode;
 }): React.JSX.Element {
 	return (
 		<body>
-			<div className="toggle">
-				<div className="toggle-theme-value">Light</div>
-				<div className="toggle-theme-value">Dark</div>
-			</div>
+			<ToggleThemeChanger />
 			<button onClick={toogleThemeColor}>Switch Theme</button>
 			{children}
 		</body>
---
+++ cv/src/components/toggle-theme-changer.tsx
@@ -0,0 +1,12 @@
+import '../css/toggle-theme-changer.css'
+function ToggleThemeChanger(): React.JSX.Element {
+	return <input type="checkbox" className="toggle" defaultChecked />
+}
+export default ToggleThemeChanger;
+
+/*
+			<div className="toggle">
+				<div className="toggle-theme-value">Light</div>
+				<div className="toggle-theme-value">Dark</div>
+			</div>
+*/
